bccc96b15f811941860bb987f2cefd1a32766624
Suyeon Lee <s2015@e-mirim.hs.kr>
2022-05-30 14:43:46 +0900

    #3 리스트 사용법

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -25,6 +25,19 @@ namespace CSClass
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble() * 10);
+
+            List<int> list = new List<int>() { 16, 2, 95, 11 };
+            /* ==
+            List<int> list = new List<int>();
+            list.Add(16);
+            list.Add(2);
+            list.Add(95);
+            list.Add(11);*/
+
+            foreach (int item in list)
+            {
+                Console.WriteLine("Count : "+ list.Count+ "\t"+item);
+            }
         }
     }
 }
